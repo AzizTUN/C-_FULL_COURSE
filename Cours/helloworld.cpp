a72452106e87cf91dc1cc23531c0fe092f34f78f
Aziz <azizabdallah495@gmail.com>
2026-07-08 09:18:10 +0100

    C++_var

---
+++ Cours/helloworld.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+int main(){
+    std::cout << "I like pizza!" << std::endl;
+    std::cout << "no terminal" << std::endl;
+    int x;
+    x=3;
+    std::cout << x << std::endl;
+    std::string age = "18";
+    std::string name = "Aziz" ;
+    bool isOk = true;
+    std::cout << name << std::endl ;
+    std::cout << name + " " + age << std::endl;
+
+    
+
+    return 0;
+}
+
