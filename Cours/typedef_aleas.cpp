9a1b5f59d442ab51ccf824665a1a5616814b1a0e
Aziz <azizabdallah495@gmail.com>
2026-07-09 09:25:11 +0100

    typedef_using

---
+++ Cours/typedef_aleas.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+//typedef std::vector<std::pair<std::string, int>> pairlist_t;
+std::vector<std::tuple<bool,int,float>> person;
+//typedef std::string string_t ;
+int main() {
+    using std::cout ;
+    using str = std::string ;
+    str name = "aziz";
+
+    cout << name << std::endl;
+    return 0;
+}
