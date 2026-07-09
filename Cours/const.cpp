46ddd5ab2b1d908b76a99778a4c916c9c8d2eec3
Aziz <azizabdallah495@gmail.com>
2026-07-09 08:39:47 +0100

    const

---
+++ Cours/const.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+int main (){
+    /*std::cout << "Aziz\n" ;
+    int x = 3;
+    std::string nom = "Aziz" ;
+    int y = 5;
+    int z = x*y;
+    std::cout << z << " "<< nom << std::endl;
+    return 0 ;*/
+    const double PI = 3.14159;
+    double raduis = 10;
+    double cir = 2*PI*raduis;
+
+    std::cout << cir << "cm" ;
+}
