e960587b0e07b0709f2f30ff21249797d6e2606b
Aziz <azizabdallah495@gmail.com>
2026-07-22 09:49:17 +0100

    bank

--- Cours/bankprog.cpp
+++ Cours/bankprog.cpp
@@ -11,33 +11,42 @@ int main () {
     int mdps = 1234;
     int mdp ;
     str answ ;
-    
+    double mn ;
+    double rea = 1000;
+
     std::cout << "donner votre mot de pass : " ;
     std::cin >> mdp ;
 
     if (mdps==mdp){
         std::cout << "welcome Aziz ! \n";
-        std::cout << "wanna dep or wth ? ";
+        std::cout << "wanna dps or wth ? ";
         std::getline(std::cin>>std::ws,answ);
-
-        switch(lower(answ)){
-            case "dep" :
-
-                break ;
-            case "wth" :
-
-                break ;
-            default :
-                std::cout << "wrong desc !" ;
-                break;
+        answ = lower(answ) ;
+        if (answ == "wth") {
+            std::cout << "how mush u wanna withdraw ? u have "<<rea <<" : ";
+            std::cin>> mn ;
+            if(mn>rea){std::cout<<"u cant , u dont have enought monney !";}
+            else{std::cout<<"ur current sold is : "<<rea-mn;}
         }
+        else if (answ == "dps") {
+            std::cout << "how mush u wanna deposit ? u have "<<rea <<" : ";
+            std::cin>> mn ;
+            std::cout<<"ur current sold is : "<<rea+mn;
+        
+        }
+    }
+        else{
+        std::cout << "wrong mdps !" ;
     }
     return 0 ;
 }
 
-str lowerr(str ch) {
+str lower(str ch) {
     str ph;
     for(char c : ch) {
-        ph += (char) ( (int) c  + 32) ;
-    }return ph ;
+        if( 'A'<= c and 'Z'>= c ){
+            ph += (char) ( (int) c  + 32) ;
+        }else{ph+=c;}
+    }
+    return ph ;
 }
